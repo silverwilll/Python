163d46e5ee1ce4ad4f2472c734d64c0eb8a52d2f
yfqian <yfqian@umich.edu>
2025-07-16 13:24:21 -0700

    add more stuff

--- classes.py
+++ classes.py
@@ -4,7 +4,12 @@ class Person:
 
     def eating(self):
         print(f"{self._name} is eating")
+    
+    def __str__(self):
+        return f"Hello I am {self._name}"
 
 p = Person("Yufei")
+text = str(p)
+print(f"text: {text}")
 p.eating()
 print(p)
